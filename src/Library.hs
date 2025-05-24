8597bf051732cb79551845170240364aa5286ba5
juanidibe <juanidb@gmail.com>
2025-05-24 07:23:58 -0300

    Punto 1 modified

--- src/Library.hs
+++ src/Library.hs
@@ -4,3 +4,17 @@ import PdePreludat
 doble :: Number -> Number
 doble numero = numero + numero
 
+-- Punto 1
+
+data Postre = unPostre{
+        sabores :: [String],
+        peso :: Number,
+        temperatura :: Number
+} deriving (Show, Eq)
+
+bizcochoBorracho :: Postre
+bizcochoBorracho = unPostre ["fruta", "crema"] 100 2
+
+-- Punto 2
+
+
